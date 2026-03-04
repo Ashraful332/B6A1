0aa9cc6eebc7b436541dc2077d1820a8e7868d16
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-03-04 17:24:44 +0600

    B6A1 is complite complitly

--- solution.js
+++ solution.js
@@ -44,3 +44,62 @@ var person1 = new Person('John Doe', 30);
 console.log(person1.getDetails());
 var person2 = new Person('Alice', 25);
 console.log(person2.getDetails());
+function filterByRating(books) {
+    var filterBooks = books.filter(function (book) { return book.rating > 4; });
+    return filterBooks;
+}
+var books = [
+    { title: 'Book A', rating: 4.5 },
+    { title: 'Book B', rating: 3.2 },
+    { title: 'Book C', rating: 5.0 },
+];
+console.log(filterByRating(books));
+function filterActiveUsers(users) {
+    return (users.filter(function (user) { return user.isActive === true; }));
+}
+var users = [
+    { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+    { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+    { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
+];
+console.log(filterActiveUsers(users));
+function printBookDetails(book) {
+    console.log("Title: ".concat(book.title, ", Author: ").concat(book.author, ", Published: ").concat(book.publishedYear, ", Available: ").concat(book.isAvailable));
+}
+var myBook = {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    publishedYear: 1925,
+    isAvailable: true,
+};
+printBookDetails(myBook);
+// Problem 7:
+function getUniqueValues(array1, array2) {
+    for (var i = 0; i < array2.length; i++)
+        if (array1.indexOf(array2[i]) == -1)
+            array1.push(array2[i]);
+    return array1;
+}
+var array1 = [1, 2, 3, 4, 5];
+var array2 = [3, 4, 5, 6, 7];
+console.log(getUniqueValues(array1, array2));
+function calculateTotalPrice(products) {
+    var total = 0;
+    for (var i = 0; i < products.length; i++) {
+        var e = products[i];
+        if (e.discount) {
+            total = total + ((e.price - ((e.price / 100) * e.discount)) * e.quantity);
+            // total = total + ((e.price - (e.price * ( e.discount/100))) * e.quantity)
+        }
+        else {
+            total = total + (e.price * e.quantity);
+        }
+    }
+    return total;
+}
+var products = [
+    { name: 'Pen', price: 10, quantity: 2 },
+    { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
+    { name: 'Bag', price: 50, quantity: 1, discount: 20 },
+];
+console.log(calculateTotalPrice(products));
--- solution.ts
+++ solution.ts
@@ -1,14 +1,14 @@
 // Problem 1:
 
-function formatValue(data:string | number | boolean) {
+function formatValue(data: string | number | boolean) {
   if (typeof (data) === "string") {
     let conString = data.toLocaleUpperCase();
     return conString
-  }else if (typeof data == "number") {
-    return data*10
-  }else if (typeof data === "boolean") {
+  } else if (typeof data == "number") {
+    return data * 10
+  } else if (typeof data === "boolean") {
     return !data
-  }else {
+  } else {
     return "error"
   }
 }
@@ -16,12 +16,12 @@ console.log(formatValue("hello"));
 
 // Problem 2:
 
-function getLength(data:string | any) {
+function getLength(data: string | any) {
   if (Array.isArray(data)) {
     return data.length;
-  }else if (typeof data === "string") {
+  } else if (typeof data === "string") {
     return data.length;
-  }else{
+  } else {
     return "error";
   }
 }
@@ -31,16 +31,16 @@ console.log(getLength([10, 20, 30, 40]));
 // Problem 3:
 
 class Person {
-  private name : string;
-  private age : number;
+  private name: string;
+  private age: number;
 
-  public constructor(name:string,age:number){
+  public constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
   }
 
-  public getDetails():string {
-    return(`Name: ${this.name}, Age: ${this.age}`)
+  public getDetails(): string {
+    return (`Name: ${this.name}, Age: ${this.age}`)
   }
 }
 const person1 = new Person('John Doe', 30);
@@ -52,12 +52,13 @@ console.log(person2.getDetails());
 // Problem 4:
 
 type books = {
-  title : string;
+  title: string;
   rating: number
 }
 
-function filterByRating(books:books[]) {
-  
+function filterByRating(books: books[]) {
+  const filterBooks = books.filter(book => book.rating > 4);
+  return filterBooks;
 }
 const books = [
   { title: 'Book A', rating: 4.5 },
@@ -66,3 +67,96 @@ const books = [
 ];
 
 console.log(filterByRating(books));
+
+// Problem 5:
+
+interface book {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+}
+
+function filterActiveUsers(users: book[]) {
+  return (users.filter(user => user.isActive === true));
+}
+
+const users = [
+  { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+  { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+  { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
+];
+
+console.log(filterActiveUsers(users));
+
+// Problem 6:
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(book: Book) {
+  console.log(`Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${book.isAvailable}`)
+}
+
+const myBook: Book = {
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+  publishedYear: 1925,
+  isAvailable: true,
+};
+
+printBookDetails(myBook);
+
+// Problem 7:
+
+function getUniqueValues(array1: number[], array2: number[]) {
+  for (let i = 0; i < array2.length; i++)
+    if (array1.indexOf(array2[i]) == -1)
+      array1.push(array2[i]);
+
+  return array1;
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
+console.log(getUniqueValues(array1, array2));
+
+// Problem 8:
+
+type products = {
+  name: string,
+  price: number,
+  quantity: number,
+  discount?: number
+}
+
+function calculateTotalPrice(products: products[]) {
+  let total = 0;
+
+  for (let i = 0; i < products.length; i++) {
+    let e = products[i];
+    if (e.discount) {
+      total = total + ((e.price - ((e.price / 100) * e.discount)) * e.quantity)
+    } else {
+      total = total + (e.price * e.quantity)
+    }
+
+  }
+
+  return total;
+}
+
+const products = [
+  { name: 'Pen', price: 10, quantity: 2 },
+  { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
+  { name: 'Bag', price: 50, quantity: 1, discount: 20 },
+];
+
+console.log(calculateTotalPrice(products));
+
+
+
