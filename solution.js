92758550feefbce3603d1d6c3ee2c3e59e0a46f3
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2026-03-04 15:00:36 +0600

    provlem 1 is solve

--- solution.js
+++ solution.js
@@ -1,25 +1,16 @@
-/*
-- This solution.ts file is an example; replace it with your own code.
-- Use the same function names and parameter names as in the problem statement.
-- Write only the code inside the function bodies and return the result.
-- Do not use any console.log statements or comments.
-*/
-var getSwallowVelocity = function (type) {
-    if (type === 'african') {
-        return 'Roughly 11 meters per second.';
+function formatValue(data) {
+    if (typeof (data) === "string") {
+        var conString = data.toLocaleUpperCase();
+        return conString;
     }
-    return "I... I don't know that!";
-};
-var isCatLiquid = function (isAsleep, containerShape) {
-    return isAsleep && containerShape !== 'none';
-};
-var Wizard = /** @class */ (function () {
-    function Wizard(name, favoriteSpell) {
-        this.name = name;
-        this.favoriteSpell = favoriteSpell;
+    else if (typeof data == "number") {
+        return data * 10;
     }
-    Wizard.prototype.castSpell = function () {
-        return "".concat(this.name, " casts ").concat(this.favoriteSpell, "!");
-    };
-    return Wizard;
-}());
+    else if (typeof data === "boolean") {
+        return !data;
+    }
+    else {
+        return "error";
+    }
+}
+console.log(formatValue("ashraful"));
